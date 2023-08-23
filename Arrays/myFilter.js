c08ad847a7d70d66b1abe7f5b64e20bb942c37bf
Sameer Shaikh <sameershaikh@Sameers-MacBook-Air.local>
2023-08-23 22:10:39 +0530

    added syntax

--- Arrays/myFilter.js
+++ Arrays/myFilter.js
@@ -1,29 +1,29 @@
 console.log('Bismillah');
 
 
-if(!Array.prototype.Myfilter){
-  Array.prototype.Myfilter = function (callbackFn){
+if (!Array.prototype.Myfilter) {
+  Array.prototype.Myfilter = function (callbackFn) {
     let results = [];
-   for(let i = 0; i< this.length; i++){
-    if(!callbackFn){
-      throw new TypeError('Callback function is required')
+    for (let i = 0; i < this.length; i++) {
+      if (!callbackFn) {
+        throw new TypeError('Callback function is required')
+      }
+      if (callbackFn(this[i], i, this)) {
+        results.push(this[i])
+      }
     }
-    if(callbackFn(this[i], i, this)){
-     results.push(this[i])
-    }
-   }
-   return results
+    return results;
   }
 
-} 
+}
 
 let arr = ['a', 'b', 'c', 'D', 'A'];
 
-let checkSchars = arr.Myfilter((item)=>{
-  if(item.toLowerCase() == 'a' ){
-   return true;
+let checkSchars = arr.Myfilter((item) => {
+  if (item.toLowerCase() == 'a') {
+    return true;
   }
- 
+
 })
 
 console.log(checkSchars)
